6b0cd0e5c20a5d68051498dfcbc78dda907ba019
Gonzalo <jara91@gmail.com>
2016-04-07 21:51:28 -0300

    HotFix de App.rb

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+module MedicallApi
+  class Application < Rails::Application
+
+    
+
   config.generators do |g|
     g.test_framework :rspec, fixture: true
     g.fixture_replacement :factory_girl, dir: 'spec/factories'
@@ -17,11 +23,6 @@ Bundler.require(*Rails.groups)
   end
 
   config.autoload_paths += %W(\#{config.root}/lib)
-
-
-
-module MedicallApi
-  class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
